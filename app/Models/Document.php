8914a042777618f5d2c51fb42ff03353228f92fc
Greg Priday <greg@siteorigin.com>
2024-08-05 11:07:33 +0200

    Remove spatie sluggable

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -5,26 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\Searchable;
-use Spatie\Sluggable\HasSlug;
-use Spatie\Sluggable\SlugOptions;
 use Spatie\Tags\HasTags;
 
 class Document extends Model
 {
-    use HasFactory, HasTags, Searchable, HasSlug;
+    use HasFactory, HasTags, Searchable;
 
     protected $fillable = [
         'title',
         'content',
         'slug',
     ];
-
-    public function getSlugOptions(): SlugOptions
-    {
-        return SlugOptions::create()
-            ->generateSlugsFrom('title')
-            ->saveSlugsTo('slug');
-    }
 
     public function getRouteKeyName()
     {
