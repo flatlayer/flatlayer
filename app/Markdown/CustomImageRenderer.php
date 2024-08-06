62b2476b3674e8d1eb06309b130bd67824e68c77
Greg Priday <greg@siteorigin.com>
2024-08-06 21:20:46 +0200

    Basics of custom markdown rendering

---
+++ app/Markdown/CustomImageRenderer.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Markdown;
+
+use App\Models\Media;
+use Illuminate\Support\Str;
+use League\CommonMark\Extension\CommonMark\Node\Inline\Image;
+use League\CommonMark\Extension\CommonMark\Renderer\Inline\ImageRenderer;
+use League\CommonMark\Renderer\NodeRendererInterface;
+use League\CommonMark\Node\Node;
+use League\CommonMark\Util\HtmlElement;
+
+class CustomImageRenderer implements NodeRendererInterface
+{
+    protected $model;
+
+    public function __construct($model)
+    {
+        $this->model = $model;
+    }
+
+    public function render(Node $node, $inlineContext = null)
+    {
+        if (!($node instanceof Image)) {
+            throw new \InvalidArgumentException('Incompatible node type: ' . get_class($node));
+        }
+
+        $url = $node->getUrl();
+
+        if (Str::startsWith($url, '/') || Str::startsWith($url, '\\')) {
+            // This is an absolute file path
+            $filename = basename($url);
+            $media = $this->model->media()->where('filename', $filename)->first();
+
+            if ($media instanceof Media) {
+                return new HtmlElement('div', ['class' => 'markdown-image'], [
+                    $media->getImgTag('100vw', ['alt' => $node->getTitle() ?: $filename])
+                ]);
+            }
+        }
+
+        // If not a local file or media not found, fall back to default rendering
+        $defaultRenderer = new ImageRenderer();
+        return $defaultRenderer->render($node, $inlineContext);
+    }
+}
---
+++ app/Markdown/CustomMarkdownRenderer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Markdown;
+
+use League\CommonMark\Environment\Environment;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use League\CommonMark\Extension\CommonMark\Node\Inline\Image;
+use League\CommonMark\MarkdownConverter;
+
+class CustomMarkdownRenderer
+{
+    protected $model;
+    protected $environment;
+    protected $converter;
+
+    public function __construct($model)
+    {
+        $this->model = $model;
+        $this->environment = new Environment();
+        $this->environment->addExtension(new CommonMarkCoreExtension());
+        $this->environment->addRenderer(Image::class, new CustomImageRenderer($this->model));
+        $this->converter = new MarkdownConverter($this->environment);
+    }
+
+    public function convertToHtml($markdown)
+    {
+        return $this->converter->convert($markdown);
+    }
+}
