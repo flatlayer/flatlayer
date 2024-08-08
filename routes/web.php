87d8a5586ed6c9787e0eedb8a9a3539eeaffe3bc
Greg Priday <greg@siteorigin.com>
2024-08-08 21:47:01 +0200

    Remove home view

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,8 @@ use App\Http\Controllers\EntryDetailController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // Treat this as a health check
+    return response()->json(['message' => 'OK']);
 });
 
 // ContentItem routes
