d2e8f7e8205891bdb8a32ecf5a91621ce56fc775
gregpriday <greg@siteorigin.com>
2024-08-08 12:07:36 +0200

    Clean up Flatlayer config

--- config/flatlayer.php
+++ config/flatlayer.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Models\Post;
-use App\Models\Document;
-
 return [
     'search' => [
         'embedding_model' => env('FLATLAYER_EMBEDDING_MODEL', 'text-embedding-3-small'),
@@ -12,23 +9,12 @@ return [
             'embed' => env('JINA_EMBED_MODEL', 'jina-embeddings-v2-base-en'),
         ],
     ],
-    'models' => [
-        Post::class => [
-            'path' => env('FLATLAYER_POST_PATH', ''),
-            'source' => env('FLATLAYER_POST_SOURCE', '**/*.md'),
-            'hook' => env('FLATLAYER_POST_HOOK', 'https://example.com/hook'),
-        ],
 
-        Document::class => [
-            'path' => env('FLATLAYER_DOCUMENT_PATH', ''),
-            'source' => env('FLATLAYER_DOCUMENT_SOURCE', '*.md'),
-            'hook' => env('FLATLAYER_DOCUMENT_HOOK', 'https://example.com/hook'),
-        ]
-    ],
     'media' => [
         'use_signatures' => env('FLATLAYER_MEDIA_USE_SIGNATURES', false),
     ],
+
     'github' => [
         'webhook_secret' => env('GITHUB_WEBHOOK_SECRET'),
-    ]
+    ],
 ];
